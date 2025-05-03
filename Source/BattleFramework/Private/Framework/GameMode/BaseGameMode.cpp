2c0213470ea6cfb34e15e27f0703ec6ab0ec63c2
ChaeKH <rbgur211@naver.com>
2025-05-03 15:15:52 +0900

    [규혁] 디버그 헬퍼 추가

---
+++ Source/BattleFramework/Private/Framework/GameMode/BaseGameMode.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Framework/GameMode/BaseGameMode.h"
+
+#include "DebugHelper.h"
+
+ABaseGameMode::ABaseGameMode()
+{
+	Debug::Print(TEXT("BaseGameMode : DebugHelper Test"));
+}
---
+++ Source/BattleFramework/Public/DebugHelper.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+//사용 예시
+	// #include "DebugHelper.h"  //사용할 코드 파일 위치에서 헤더파일 포함
+
+	// FString Msg = FString::Printf(출력할 포맷 설정해서);
+	// 예시 : FString Msg = FString::Printf(TEXT("PlayerLocation.X : %f"). PlayerLocation.X);
+	// Debug::Print(Msg);  요렇게도 가능합니다~!
+
+	// Debug::Print(TEXT("출력할 메시지"));
+	// Debug::PrintLog(TEXT("출력할 메시지"));
+	// Debug::PrintError(TEXT("출력할 메시지"));
+	// Debug::PrintDM(TEXT("출력할 메시지"));
+
+	//스크린 디버그 메시지에 색 지정 or Key값 필요한 경우
+	// Debug::Print(TEXT("출력할 메시지"), FColor::원하는색상);
+	// Debug::Print(TEXT("출력할 메시지"), FColor::원하는색상, 1);
+	// Debug::PrintDM(TEXT("출력할 메시지"), FColor::원하는색상);
+	// Debug::PrintDM(TEXT("출력할 메시지"), FColor::원하는색상, 1);
+
+//왜 쓰냐?
+//매번 UE_LOG(LogTemp, ~~~~~~) 쓰기 귀찮잖아요
+// GEngine->AddOnScreenDebugMessage(~~~~~~~~~~~~) 도 그렇고
+namespace Debug
+{
+	//로그와 스크린디버그메시지 동시 출력
+	static void Print(const FString& Msg, const FColor& Color = FColor::Green, int32 Inkey = -1)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(Inkey, 7.f, Color, Msg);
+
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
+		}
+	}
+
+	//로그만 출력(Warning)
+	static void PrintLog(const FString& Msg)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
+	}
+
+	//로그만 출력(Error)
+	static void PrintError(const FString& Msg)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s"), *Msg);
+	}
+
+	//스크린디버그메시지만 출력
+	static void PrintDM(const FString& Msg, const FColor& Color = FColor::Green, int32 Inkey = -1)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(Inkey, 5.f, Color, Msg);
+		}
+	}
+}
